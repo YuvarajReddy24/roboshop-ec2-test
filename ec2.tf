c4176c17e678f26d847f1d8716a84b931b7bfe6e
yuvaraj <yuvarajreddy24@gmail.com>
2025-10-21 21:03:18 +0530

    .

--- ec2.tf
+++ ec2.tf
@@ -11,3 +11,11 @@ sg_ids = var.sg_ids
 instance_type = var.instance_type
 tags = var.tags
 }
+
+output "public_ip" {
+    value = module.catalogue.public_ip
+}
+
+output "private_ip" {
+    value = module.catalogue.private_ip
+}
